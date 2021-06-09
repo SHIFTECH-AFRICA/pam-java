868718700b035007f2192c5f71356c86bfd8828a
dev-techguy <dev.techguy@gmail.com>
2021-06-09 11:50:02 +0300

    :package: Getting started

---
+++ src/main/java/com/shiftechafrica/pamjava/PamJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shiftechafrica.pamjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftechafrica/pamjava/PamJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftechafrica.pamjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
